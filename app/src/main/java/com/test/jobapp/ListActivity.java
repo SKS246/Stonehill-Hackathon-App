fe27879057a28d5beaa663407f091b67719b7a34
Sks301 <soham.sinha@oberoi-is.net>
2022-03-04 16:26:20 +0530

    changes in ListActivity.java

--- app/src/main/java/com/test/jobapp/ListActivity.java
+++ app/src/main/java/com/test/jobapp/ListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.math.BigDecimal;
+
 public class ListActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,30 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
     }
+};
+
+private class prepareItems() {
+    for(
+    int i = 0;
+    i< 50;i++)
+
+    {
+        Items items = new Items("Item" + i, 20 + i);
+        BigDecimal itemsList = null;
+        itemsList.add(items);
+    }
+    recyclerviewItemAdapter = new RecyclerviewItemAdapter(itemsList);
+recyclerView.setHasFixedSize(true);
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+recyclerView.setLayoutManager(layoutManager);
+recyclerView.setItemAnimator(new DefaultItemAnimator());
+recyclerView.setAdapter(recyclerviewItemAdapter);
+
+
 }
+
+
+
+
+
 
--- app/src/main/java/com/test/jobapp/RecyclerviewItemAdapter.java
+++ app/src/main/java/com/test/jobapp/RecyclerviewItemAdapter.java
@@ -55,3 +55,5 @@ public class RecyclerviewItemAdapter extends RecyclerView.Adapter<RecyclerviewIt
         }
     }
 }
+
+
